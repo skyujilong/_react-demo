28b0dc4132647e2f0b64826c10cf0f057a960f8c
skyujilong <skyujilong@163.com>
2016-03-22 16:33:17 +0800

    add note

--- normal/js/tutorial6.js
+++ normal/js/tutorial6.js
@@ -38,13 +38,21 @@ var Comment = React.createClass({
         );
     },
     componentDidMount:function(){
-
-        setTimeout(function(){
+        //在组件初始化的时候执行一次，可以用来初始化 state 当内置对象state发生改变的时候自动重绘当前组件。
+        // （可以用来请求服务器获取对应的数据）
+        /*setTimeout(function(){
             this.setState({data:[
                 {"author": "Pete Hunt", "text": "This is one comment"},
                 {"author": "Jordan Walke", "text": "This is *another* comment"}
             ]});
-        }.bind(this),3000);
+        }.bind(this),3000);*/
+        //箭头函数，会获取器所在上下文的this作为自己的this值。 下面的代码等同于上面的代码
+        setTimeout(()=>{
+            this.setState({data:[
+                {"author": "Pete Hunt", "text": "This is one comment"},
+                {"author": "Jordan Walke", "text": "This is *another* comment"}
+            ]});
+        },3000);
     }
 });
 
