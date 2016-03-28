dda1cbfb840e04d3b2806d14e3cb86fcbadc01a5
skyujilong <skyujilong@163.com>
2016-03-28 15:40:00 +0800

    add new demo for es6

---
+++ normal/js/tutorial10.js
@@ -0,0 +1,23 @@
+//解构赋值
+var foo = [1,2,3,4];
+var [one,two,three] = foo;
+console.log(one);
+console.log(two);
+console.log(three);
+//对应赋值为 one=1,two=2,three=3
+console.log('-----------------------------------------------');
+var [a,,b,c] = foo;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log('-----------------------------------------------');
+//交换变量
+var x = 1, y = 2;
+[x,y] = [y,x];//后面的解析为数组
+console.log('x:%d',x);
+console.log('y:%d',y);
+console.log('-----------------------------------------------');
+//解构对象
+var o = {xx:20,yy:true};
+var {xx,yy} = o;//属性名字的一样才能解构出来
+console.log('xx:%o;yy:%o',xx,yy);
